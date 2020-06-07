d90ac5143574a62645c0975817058bc6b70da14a
shadowchin <shadowchin@gmail.com>
2020-06-07 15:22:57 +0800

    configured static file settings

--- crm/wsgi.py
+++ crm/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for crm project.
+WSGI config for crmapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crmapp.settings")
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
