6402bd8a76538555fdaa3216dbd0d09cfbc40064
shadowchin <shadowchin@gmail.com>
2020-06-07 21:43:11 +0800

    Reconfigured templates, URLs, Views & static !!

--- crm/urls.py
+++ crm/urls.py
@@ -13,20 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
+from django.contrib import admin
+from django.urls import path
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
-from django.conf.urls import patterns, include, url
 
-from marketing.views import HomePage
+#from marketing.views import HomePage
+from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
+    # Admin site
+    path('admin/', admin.site.urls),
 
     # Marketing pages
-    url(r'^$', HomePage.as_view(), name="home"),
+    path('marketing/', views.HomePage.as_view(), name="home"),
 
     # Subscriber related URLs
 
@@ -45,4 +47,4 @@ urlpatterns = patterns('',
 
     # Communication related URLs
 
-)
+]
